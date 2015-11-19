afdac2958b9d9ddddfb1c23edaa4db6b56d38fcc
Remco Tolsma <info@remcotolsma.nl>
2015-11-19 13:20:18 +0100

    Extend from iDEAL Advanced v3 library.

--- src/Integration.php
+++ src/Integration.php
@@ -1,22 +1,10 @@
 <?php
 
-class Pronamic_WP_Pay_Gateways_FrieslandBank_IDealZakelijkPlusV3_Integration {
+class Pronamic_WP_Pay_Gateways_FrieslandBank_IDealZakelijkPlusV3_Integration extends Pronamic_WP_Pay_Gateways_IDealAdvancedV3_AbstractIntegration {
 	public function __construct() {
 		$this->id         = 'frieslandbank-ideal-zakelijk-plus-v3';
 		$this->name       = 'Friesland Bank - iDEAL Zakelijk Plus - v3';
 		$this->provider   = 'frieslandbank';
 		$this->deprecated = true;
-	}
-
-	public function get_config_factory_class() {
-		return 'Pronamic_WP_Pay_Gateways_IDealAdvancedV3_ConfigFactory';
-	}
-
-	public function get_config_class() {
-		return 'Pronamic_WP_Pay_Gateways_IDealAdvancedV3_Config';
-	}
-
-	public function get_gateway_class() {
-		return 'Pronamic_WP_Pay_Gateways_IDealAdvancedV3_Gateway';
 	}
 }
