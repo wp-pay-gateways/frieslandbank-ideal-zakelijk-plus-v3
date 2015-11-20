5998526a7f320e7833c0bfa1e7841e076074d9c4
Remco Tolsma <info@remcotolsma.nl>
2015-11-20 12:19:25 +0100

    Extend from v3.

--- src/Config.php
+++ src/Config.php
@@ -8,7 +8,7 @@
  * @author Remco Tolsma
  * @version 1.0.0
  */
-class Pronamic_WP_Pay_Gateways_FrieslandBank_IDealZakelijkPlusV3_Config extends Pronamic_WP_Pay_Gateways_IDealAdvanced_Config {
+class Pronamic_WP_Pay_Gateways_FrieslandBank_IDealZakelijkPlusV3_Config extends Pronamic_WP_Pay_Gateways_IDealAdvancedV3_Config {
 	public function get_payment_server_url() {
 		return 'https://idealkassa.frieslandbank.nl/ideal/iDEALv3';
 	}
