bd9b103e868094ada5b86b6a54f7e0c44c5d16e2
Reüel van der Steege <reuel@pronamic.nl>
2016-03-03 16:48:12 +0100

    Update integration name

--- src/Integration.php
+++ src/Integration.php
@@ -12,7 +12,7 @@
 class Pronamic_WP_Pay_Gateways_FrieslandBank_IDealZakelijkPlusV3_Integration extends Pronamic_WP_Pay_Gateways_IDealAdvancedV3_AbstractIntegration {
 	public function __construct() {
 		$this->id         = 'frieslandbank-ideal-zakelijk-plus-v3';
-		$this->name       = 'Friesland Bank - iDEAL Zakelijk Plus - v3';
+		$this->name       = 'Friesland Bank - iDEAL Zakelijk Plus (v3)';
 		$this->provider   = 'frieslandbank';
 		$this->deprecated = true;
 	}
